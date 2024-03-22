68482b97f70e09391de30002cba8ba58ada9567d
Andy <andy@Andys-MacBook-Pro.local>
2024-03-22 12:24:08 -0400

    code

---
+++ components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const num = ref(0)
+
+const onClick = () => {
+  num.value++
+}
+</script>
+
+
+<template>
+  <p>{{ num }}</p>
+  <button @click="onClick">click me</button>
+</template>
---
+++ pages/index.server.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Server Only Page</h1>
+  <div>
+    <Counter nuxt-client></Counter>
+  </div>
+</template>
